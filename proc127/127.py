f1cc991cfc09f680014c4894ae5311209239a34c
ayaanabrol <69078639+ayaanabrol@users.noreply.github.com>
2022-03-10 10:13:53 +0530

    Add files via upload

---
+++ proc127/127.py
@@ -0,0 +1,27 @@
+import bs4 as   BeautifulSoup
+import requests
+import pandas as pd
+START_URL = ("https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars")
+
+wikipedia = requests.get(START_URL)
+soup= BeautifulSoup(wikipedia.text,"html.parser")
+temp_list=[]
+
+for tr in soup.find("table").find_all("tr"):
+    td=tr.find_all("td")
+    row=[i.text.rstrip()for i in td]
+    temp_list.append(row)
+
+name=[]
+distance=[]
+mass=[]
+radius=[]
+
+for i in range(1,len(temp_list)):
+    name.append(temp_list[i][1])
+    distance.append(temp_list[i][3])
+    mass.append(temp_list[i][5])
+    radius.append(temp_list[i][6])
+
+df=pd.DataFrame(list(zip(name,distance,mass,radius)),columns=["Star_name","Distance","Mass","Radius"],)
+df.to_csv("data.csv")
